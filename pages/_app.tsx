1e0a1173067062668e1fa8445e937becdcdc9057
bluepadu <padum3955@gmail.com>
2022-12-12 05:45:54 -0800

    Update: movie responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Movie App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Provider store={store}>
         <Layout>
